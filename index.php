b00f85cc5045eba4ce29b922e799f52a8e94fd81
unknown <Admin@.(none)>
2011-08-03 02:15:17 +0400

    Added everything

---
+++ index.php
@@ -0,0 +1,28 @@
+<?
+
+require_once '/twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array('debug'=>true/*, 'cache'=>'cache'*/));
+
+$pages = array(
+	'/'=>'index',
+	'/index.html'=>'index',
+	'/index.php'=>'index',
+	'index'=>'index',
+	'/records/risunki'=>'risunki'
+);
+
+$query = $_SERVER['REQUEST_URI'];
+
+if (isset($pages[$query])) {
+
+	$template_name = $pages[$query] . '.html';
+
+	$template = $twig->loadTemplate($template_name);
+	echo $template->render(array());
+
+} else {
+	echo '404';
+}
