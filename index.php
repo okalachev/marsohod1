8876fcc81c5bbc6b8bc9ffde85d3e8be33720aaf
oleg <okalachev@gmail.com>
2011-10-09 23:13:53 +0400

    Fixes

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ include "Renderer.php";
 include "Site.php";
 
 $renderer = new Renderer();
+// Add metrica code variable, if is's set
+if (isset($settings['metrica'])) $renderer->add_variable('metrica', $settings['metrica']);
+// Main controller
 $site = new Site($renderer);
 
 $router = new Router();
@@ -15,14 +18,14 @@ $router = new Router();
 $router->add_route(array('/', '/index.html', '/index.php', '/index/'), $site, 'page', 'index');
 // Simple pages
 $router->add_route('/base/', $site, 'page', 'pano');
-$router->add_route('/band/', $site, 'page', 'band');
+$router->add_route('/band/', $site, 'photo_page', array('band', 'band'));
 $router->add_route('/calls/', $site, 'page', 'calls');
 $router->add_route('/video/', $site, 'page', 'video');
 // Albums
 $router->add_route('/records/risunki/', $site, 'album', 'risunki');
-$router->add_route('/records/calendar/', $site, 'album', 'calendar');
+$router->add_route('/records/demo2/', $site, 'album', 'demo2');
 // Album XMLs
 $router->add_route('/risunki.xml', $site, 'album_xml', 'risunki');
-$router->add_route('/calendar.xml', $site, 'album_xml', 'calendar');
+$router->add_route('/demo2.xml', $site, 'album_xml', 'demo2');
 
-$router->route($_SERVER['REQUEST_URI']);
+$router->route();
