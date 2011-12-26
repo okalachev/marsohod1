342ef8b9cb110d41496ff6e7af7ddf3847dd8aea
oleg <okalachev@gmail.com>
2011-12-27 01:25:41 +0300

    Fixes to photos script, using Windows-version of covert in cygwin, thumbnails' vertical size parameter

--- update_photo_folder.sh
+++ update_photo_folder.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 # Обработать папку с фотографиями (сделать превьюшки, резайзнуть)
-# Параметр - название папки с фотографиями в /media/__photo_source
+# 1 параметр - название папки с фотографиями в /media/__photo_source
+# 2 параметр - размер провеьюшек по вертикали (по умолчанию 120)
+
+if [ "$1" == "" ]
+then
+	echo "Missing photos directory name!"
+	exit 1
+fi
+
+if [ `uname -o` == "Cygwin" ]
+then
+	# !TODO: fix
+	shopt -s expand_aliases
+	alias convert="c:/Soft/ImageMagick/convert.exe"
+fi
+
+VSIZE=120
+if [ $2 ]
+then
+	VSIZE=$2
+fi
+
+echo "Will be processing the folder "$1". The vertical size of thumbnails will be $VSIZE."
 
 # Удаляем yaml-карту папки
 rm yaml/auto/$1.photo.yaml
 
 # Удяляем все фотки из папки
-rm "media/photo/$1/*"
-rm "media/thumb/$1/*"
+rm -rf "media/photo/$1"
+rm -rf "media/thumb/$1"
+mkdir "media/photo/$1"
+mkdir "media/thumb/$1"
 
 # Входим в папку исходных фотографий
 cd media/__photo_source/$1/
@@ -18,7 +42,7 @@ do
 	echo "Processing $f: original..."
 	convert -resize x600 "$f" "../../photo/$1/$f"
 	echo "Thumbnail..."
-	convert -size x120 -resize x120 "$f" "../../thumb/$1/$f"
+	convert -size x$VSIZE -resize x$VSIZE "$f" "../../thumb/$1/$f"
 	echo "Writing to YAML-file..."
 	echo "$1/$f" >> ../../../yaml/auto/$1.photo.yaml
 	echo "Done"
