a252c9da53ccc6a0cc99804089cc9d038cfab776
oleg <okalachev@gmail.com>
2012-10-20 23:01:47 +0400

    Refactoring of the fisheye effect

--- media/js/fisheye.js
+++ media/js/fisheye.js
@@ -12,41 +12,49 @@
 
         var $this = $(this),
             $items = $this.find(opts.items),
-            $title = $('<div/>').addClass('fisheye-title').hide().appendTo('body');
+            $title = $('<div/>').addClass('fisheye-title').hide().appendTo('body'),
+            $selected = $();
 
+        // Showing title
+        $this.on('mouseenter', opts.items, function() {
+            var $item = $(this);
+            $selected = $item;
+            if ($item.attr('title')) {
+                $title.html($item.attr('title')).show();
+            }
+        });
+
+        // Hiding title
+        $this.on('mouseout', opts.items, function() {
+            $title.hide();
+            $selected = $();
+        });
+
+        // The fisheye effect
         $(document).mousemove(function(e) {
             $items.each(function() {
                 var $item = $(this),
                     position = $item.offset(),
                     itemX = position.left + $item.width() / 2,
                     itemY = position.top + $item.height() / 2,
+                    xDist = itemX - e.pageX,
+                    yDist = itemY - e.pageY,
                     distance = Math.sqrt(
-                        Math.pow(itemX - e.pageX, 2) + Math.pow((itemY - e.pageY) * opts.yCoeff, 2)
+                        Math.pow(xDist, 2) + Math.pow(Math.max(Math.abs(yDist), opts.from / 2) * opts.yCoeff, 2)
                     ), // Distance between the center of the item and the pointer
                     mult = distance < opts.dist ?
                         ((opts.dist - distance) / opts.dist) * opts.coeff : 0,
                     px = opts.from + (mult * (opts.to - opts.from));
                 $(this)
                     .css('height', px +'px')
-                    .css('margin-top', -(px - opts.from) + 'px')
+                    .css('margin-top', -(px - opts.from) + 'px');
                     //.css('box-shadow', '0 0 ' + (mult * 20) + 'px 0 silver')
             });
+            $title
+                .css('top', $selected.offset().top + $selected.height())
+                .css('left', $selected.offset().left + $selected.width() / 2 - $title.outerWidth() / 2);
         });
 
-        $this.on('mouseenter', opts.items, function() {
-            var $item = $(this);
-            if ($item.attr('title')) {
-                $title
-                    .html($item.attr('title'))
-                    .css('top', $item.offset().top + $item.height())
-                    .show()
-                    .css('left', $item.offset().left + $item.width() / 2 - $title.width() / 2); // TODO
-            }
-        });
-
-        $this.on('mouseout', opts.items, function() {
-            $title.hide();
-        });
     }
 
 })(jQuery);
