158ebc63b68e93c6c99cbcd6413819c49334a0fb
Oleg Kalachev <okalachev@gmail.com>
2021-08-03 06:48:30 +0300

    Disable debug toolbar to generate the site

--- src/local_settings.py
+++ src/local_settings.py
@@ -23,13 +23,13 @@ STATICFILES_FINDERS = (
 
 INSTALLED_APPS += (
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
 )
 
 INTERNAL_IPS = ('127.0.0.1',)
 
 MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ENABLE_AUTO_THUMBS = True
